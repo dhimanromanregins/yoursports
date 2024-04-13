cd07089e0b0756f9ee02765e3bddc3aa4ba8023a
sahil Dhiman <sahildhiman98765@gmail.com>
2024-04-13 16:13:10 +0530

    updates

--- yoursport_admin/Authentication/admin.py
+++ yoursport_admin/Authentication/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import CustomUser, Pricing, Contact,PasswordResetToken, FootballTeam,FAQ
+from .models import CustomUser, Pricing, Contact,PasswordResetToken, FootballTeam,FAQ, EndUser, EndUserDetail
 
 # Register your models here.
 admin.site.register(CustomUser)
@@ -8,3 +8,5 @@ admin.site.register(Contact)
 admin.site.register(PasswordResetToken)
 admin.site.register(FootballTeam)
 admin.site.register(FAQ)
+admin.site.register(EndUser)
+admin.site.register(EndUserDetail)
